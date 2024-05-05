9bba2a2861f7573c8b6c08cd5ac53c595fa3df8b
Kiliroy <borges.mathieu@gmail.com>
2024-05-05 09:13:25 +0200

    Initialized database connection

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-
+// Charger toutes les dépendances Composer
 require_once __DIR__ . '/vendor/autoload.php';
+
+// Charger le gestionnaire de base de données
+use App\Database;
+
+// Initialiser la connexion à la base de données
+// Ici, étant dans le point d'entrée de l'application, on instancie la classe Database
+// Afin d'avoir une seule connexion à la base de données, pour toute la duree de l'application
+$database = Database::getInstance();
